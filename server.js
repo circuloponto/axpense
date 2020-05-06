99b9dfa8b8d508bf276656712c51a0625bd4af63
Nuno Cortez <nucortez1@gmail.com>
2020-05-06 19:55:03 +0100

    no morgan

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
-const PORT = process.env.PORT || 5000;
+
 connectDB();
 
 const transactions = require('./routes/transactions');
@@ -15,9 +15,9 @@ const app = express();
 
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
+/* if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
-}
+} */
 
 app.use('/api/v1/transactions', transactions);
 
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
