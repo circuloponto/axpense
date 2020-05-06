0ece9613f0cc41bb4c0de81ddc41f56bab40d514
Nuno Cortez <nucortez1@gmail.com>
2020-05-06 18:21:32 +0100

    port db

---
+++ client/build/precache-manifest.d39e4d12a7a77d489d59f43a5ea1597c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2db8e88ca96f15e9db7ab1069da585f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e01a546d2e0f87455972",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01a546d2e0f87455972",
+    "url": "/static/js/main.fdc399d5.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
-
+const PORT = process.env.PORT || 5000;
 connectDB();
 
 const transactions = require('./routes/transactions');
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
